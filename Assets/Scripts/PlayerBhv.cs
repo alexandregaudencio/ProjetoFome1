65555d92c060eee96f572358ca0b4a25cc459e12
Alexandre Gaudêncio <alexandre.gtpjr@gmail.com>
2021-01-15 19:32:37 -0300

    acertos na movimentação e touch

--- Assets/Scripts/PlayerBhv.cs
+++ Assets/Scripts/PlayerBhv.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+
 
 //Component "Rigidbody2D" Indispensável pra esse script
 [RequireComponent(typeof(Rigidbody2D))]
@@ -9,15 +9,9 @@ public class PlayerBhv : MonoBehaviour
 {
 
     private Rigidbody2D rigidbody;
-    private float directionH = 0f;
+    private float HorizontalDirection = 0f;
     public float movementSpeed = 2f;
-
-    int esquerda;
-    int direita;
-    int comecar = 0;
-
-
-    private bool touched = false;
+    float movementAcel = 0.1f;
 
     void Start()
     {
@@ -45,83 +39,50 @@ public class PlayerBhv : MonoBehaviour
 
      void startGame()
     {
-        // if (comecar == 1)
-        // {
-        //     Vector2 velocity = rigidbody.velocity;
-        //     velocity.y = VariableControler.initialImpulse;
-        //     rigidbody.velocity = velocity;
-        //     VariableControler.gameStarted = true;
-
-        // }
-
-
-
         
-         if (Input.touchCount > 0)
+        if (Input.touchCount > 0)
         {
-             Vector2 velocity = rigidbody.velocity;
-             if(!VariableControler.gameStarted)
+            Vector2 velocity = rigidbody.velocity;
+            if(!VariableControler.gameStarted)
             {
-
-                 velocity.y = VariableControler.initialImpulse;
-                 rigidbody.velocity = velocity;
-                 VariableControler.gameStarted = true;
+                velocity.y = VariableControler.initialImpulse;
+                rigidbody.velocity = velocity;
+                VariableControler.gameStarted = true;
             }
         }
         
     }
 
      void TouchMovement()
-     {
+    {
         rigMovAcel();
-        Debug.Log(Input.touchCount);
-
+        
         if (Input.touchCount > 0) {
             Touch touch = Input.GetTouch(0);
-             if (touch.position.x < Screen.width/2)
-                {
-                    directionH = -1f;
-                    
-                }
+            if (touch.position.x < Screen.width/2)
+            {
+                SetMovementDirection(-1);
 
-                else if (touch.position.x > Screen.width/2)
-                {
-                    directionH = 1f;
-                    
+            }
+            else if (touch.position.x > Screen.width/2)
+            {
 
-                }
-            // if (touch.phase == TouchPhase.Began || touch.phase == TouchPhase.Moved )
-            // {
-            //     if (touch.position.x < Screen.width/2)
-            //     {
-            //         directionH = -1f;
-                    
-            //     }
-
-            //     else if (touch.position.x > Screen.width/2)
-            //     {
-            //         directionH = 1f;
-                    
-            //     }
-            // }
-            // if (touch.phase == TouchPhase.Ended)
-            // {
-
-            //     directionH = 0f;
-                
-            // }
+                SetMovementDirection(1);
+            }
         } else {
-            directionH = 0f;
+            SetMovementDirection(0);
         }
-      
+    }
 
+    float SetMovementDirection(int vectorUnit) {
+        return   HorizontalDirection = Mathf.Lerp( HorizontalDirection, vectorUnit, movementAcel);
     }
 
      void rigMovAcel()
      {
-         Vector2 velocity = rigidbody.velocity;
-         velocity.x = directionH * movementSpeed;
-         rigidbody.velocity = velocity;
+        Vector2 velocity = rigidbody.velocity;
+        velocity.x = HorizontalDirection * movementSpeed;
+        rigidbody.velocity = velocity;
 
      }
 
@@ -132,12 +93,5 @@ public class PlayerBhv : MonoBehaviour
     //         LevelGenerator.spawnItem();
     //     }
     // }
-
-
-//apaga não, por hora...  -> (APAGA NAO DEPOIS PODE SERVIR DE CONSULTA PRA OUTROS PROJETOS :) )
-// touchPos.y = Mathf.Lerp( touchPos.y, 1, lerpSpeed );
-
-
-
 
 }
